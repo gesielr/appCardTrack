86b6ff7dd899c4ca6af324f3a9a820de2bba1300
gesielr <carlosgesiell@gmail.com>
2025-06-10 11:59:33 -0300

    feat: ajusta menu rápido e navegação da HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+// ✅ Correção recomendada
+import React, { JSX } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 
-export default function App() {
+const App: React.FC = (): JSX.Element => {
   return (
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
   );
-}
+};
+
+export default App;
